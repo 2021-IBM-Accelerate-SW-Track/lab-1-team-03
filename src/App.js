3aa4c70ccbf0f616a4ef31660439bf6f2e7a2cc0
daniel2908 <40810843+daniel2908@users.noreply.github.com>
2021-06-14 13:21:23 -0400

    added basic todolist functionality
    - missing stuff like delete, edit todos and creation time
    - probably need to reformat the code and put this stuff in a new folder
    - you might need to install some npm packages ()

--- src/App.js
+++ src/App.js
@@ -1,12 +1,79 @@
 import Header from "./component/header"
+import React, {useState} from "react"
+import ReactDOM from "react-dom"
+import {TextField} from "@material-ui/core";
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import {styled} from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    <Header/>
-    </div>
-  );
+const gridItem = styled(Paper)(({theme}) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    alignItems: 'center',
+}));
+
+function ItemInput(props) {
+    const [item, setItem] = useState("");
+    const [items, setItems] = useState([]);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        const newItem = {
+            id: new Date().getTime(),
+            text: item,
+        };
+        setItems([...items].concat(newItem));
+        setItem("");
+    }
+
+    const itemlist = items.map((item) => (
+        <div key={item.id}>
+            <Box sx={{flexGrow: 1}}>
+                <Grid container spacing={0}>
+                    <Grid item xs={12}>
+                        <Item>{item.text} <IconButton onClick={() => null} className="trash">
+                            <DeleteIcon/>
+                        </IconButton></Item>
+                    </Grid>
+                </Grid>
+            </Box>
+        </div>
+    ))
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <TextField
+                    label="What do you want to do?"
+                    color="secondary"
+                    type="text"
+                    value={item}
+                    onChange={(e) => setItem(e.target.value)}
+                    fullWidth
+                    required={true}
+                />
+            </form>
+            <div>{itemlist}</div>
+        </>
+    );
 }
+
+function App() {
+
+    return (
+        <div className="App">
+            <h1>To-Do list</h1>
+            <ItemInput/>
+        </div>
+    );
+}
+
 
 export default App;
